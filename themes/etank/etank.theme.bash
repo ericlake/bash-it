51a7a306ca347e60a82249302a5658702edaddbd
Eric Lake <ericlake@gmail.com>
2013-01-08 12:39:20 -0500

    added dot_bash_profile and etank theme based on candy

---
+++ themes/etank/etank.theme.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+function prompt_command() {
+    PS1="${red}\u${reset_color}@${green}\h ${blue}\T ${reset_color}${white}\w${reset_color}$(scm_prompt_info)${blue} →${bold_blue} ${reset_color}";
+}
+
+PROMPT_COMMAND=prompt_command;
