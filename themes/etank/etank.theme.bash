574fcc01e592dd274dba530ad81d69019b9c8cca
Eric Lake <elake@lexmark.com>
2013-01-08 13:48:14 -0500

    making it prettier and adding a second line

--- themes/etank/etank.theme.bash
+++ themes/etank/etank.theme.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 function prompt_command() {
-    PS1="${red}\u${reset_color}@${green}\h ${blue}\T ${reset_color}${white}\w${reset_color}$(scm_prompt_info)${blue} →${bold_blue} ${normal}";
+    PS1="${red}\u${reset_color}@${green}\h ${purple}\T ${reset_color}${white}\w${yellow}$(scm_prompt_info)${blue}\n→${bold_blue} ${normal}";
 }
 
 PROMPT_COMMAND=prompt_command;
