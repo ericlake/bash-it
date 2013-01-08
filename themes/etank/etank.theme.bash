07e5cfc3fe260e47b24066b170d2b0949377ee36
Eric Lake <ericlake@gmail.com>
2013-01-08 13:10:27 -0500

    prettied up the etank theme

--- themes/etank/etank.theme.bash
+++ themes/etank/etank.theme.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 function prompt_command() {
-    PS1="${red}\u${reset_color}@${green}\h ${blue}\T ${reset_color}${white}\w${reset_color}$(scm_prompt_info)${blue} →${bold_blue} ${reset_color}";
+    PS1="${red}\u${reset_color}@${green}\h ${blue}\T ${reset_color}${white}\w${reset_color}$(scm_prompt_info)${blue} →${bold_blue} ${normal}";
 }
 
 PROMPT_COMMAND=prompt_command;
